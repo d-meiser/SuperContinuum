105187d9dc45b03e594f1fd6dd04df38d0a49606
Dominic Meiser <dmeiser@txcorp.com>
2015-08-05 14:53:53 -0600

    Add utility for downloading, building, and installing cgreen.

---
+++ utilities/get_cgreen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! -d cgreen-code ]; then
+  svn checkout svn://svn.code.sf.net/p/cgreen/code/trunk cgreen-code
+else
+  cd cgreen-code
+  svn up
+  cd -
+fi
+cd cgreen-code
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:BOOL=../../cgreen ../cgreen
+make -j2
+make install
+
+
