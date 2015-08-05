89b4d726cc3888d5a306f6b663884c02f5a074f9
Dominic Meiser <dmeiser@txcorp.com>
2015-08-05 14:32:36 -0600

    Switch over from gtest to cgreen.

--- include/fft.h
+++ include/fft.h
@@ -1,7 +1,11 @@
 #ifndef FFT_H
 #define FFT_H
 
+#include <petscdm.h>
+#include <petscmat.h>
+
 namespace npp {
+  Mat createFFTFromDMDA(DM da);
 };
 
 #endif
--- tests/test_FFT.cpp
+++ tests/test_FFT.cpp
@@ -1,3 +1,9 @@
-#include <gtest/gtest.h>
+#include <cgreen/cgreen.h>
+using namespace cgreen;
 
-TEST(Dummy, Foo) { EXPECT_TRUE(true); }
+#include <fft.h>
+
+int main(int argc, char **argv) {
+  TestSuite *suite = create_test_suite();
+  return run_test_suite(suite, create_text_reporter());
+}
