50c440405c62d1422c2119bc369f7967d3138b53
Dominic Meiser <dmeiser@txcorp.com>
2015-08-07 10:48:03 -0600

    Add a script for downloading and building petsc.

---
+++ utilities/get_petsc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+PETSC_TAR_BALL=petsc-lite-3.6.1.tar.gz
+PETSC_RELEASE_URL=http://ftp.mcs.anl.gov/pub/petsc/release-snapshots
+if [ ! -f $PETSC_TAR_BALL ]; then
+  wget $PETSC_RELEASE_URL/$PETSC_TAR_BALL
+fi
+if [ ! -d petsc-3.6.1 ]; then
+  tar xfz $PETSC_TAR_BALL
+fi
+cd petsc-3.6.1
+PETSC_ARCH=double-dbg PETSC_DIR=`pwd` ./configure \
+  --with-mpi=0 \
+  --with-x=0 \
+  --with-ssl=0 \
+  --with-fortran-kernels=0 \
+  --with-pthread=0 \
+  --download-fftw=1 \
+  --with-mpi=1 \
+  --with-mpiuni-fortran-binding=0 \
+  --with-fortran-interfaces
+make PETSC_DIR=`pwd` PETSC_ARCH=double-dbg all
+cd -
+
