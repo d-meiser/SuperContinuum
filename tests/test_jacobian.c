9c0fd05b5b7609dd0e579624192235aa8fcbe42a
Dominic Meiser <dmeiser@txcorp.com>
2015-08-15 11:53:19 -0600

    Add tests for constant part of jacobian.

--- tests/test_jacobian.c
+++ tests/test_jacobian.c
@@ -18,14 +18,37 @@ with SuperContinuum.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <cgreen/cgreen.h>
 #include <jacobian.h>
+#include <test_utils.h>
 #include <petscdmda.h>
 
 static const char help[] = "Unit tests for Jacobian construction methods.";
+
+Ensure(can_build_jacobian)
+{
+  struct FDFixture fixture;
+  PetscErrorCode ierr;
+  ierr = scFdSetup(&fixture);CHKERRV(ierr);
+  ierr = scJacobianBuildConstantPart(fixture.da, fixture.m, PETSC_FALSE);CHKERRV(ierr);
+  assert_that(ierr, is_equal_to(0));
+  ierr = scFdTeardown(&fixture);CHKERRV(ierr);
+}
+
+Ensure(can_build_jacobian_fourth_order)
+{
+  struct FDFixture fixture;
+  PetscErrorCode ierr;
+  ierr = scFdSetup(&fixture);CHKERRV(ierr);
+  ierr = scJacobianBuildConstantPart(fixture.da, fixture.m, PETSC_TRUE);CHKERRV(ierr);
+  assert_that(ierr, is_equal_to(0));
+  ierr = scFdTeardown(&fixture);CHKERRV(ierr);
+}
 
 int main(int argc, char **argv)
 {
   PetscInitialize(&argc, &argv, NULL, help);
   TestSuite *suite = create_test_suite();
+  add_test(suite, can_build_jacobian);
+  add_test(suite, can_build_jacobian_fourth_order);
   int result = run_test_suite(suite, create_text_reporter());
   PetscFinalize();
   return result;
